80aeb06247569707fd8e440583a40dad44f38f5a
romil rõbtšenkov <romilrobtsenkov@gmail.com>
2016-02-15 10:20:09 +0200

    added prev lesson files

---
+++ app.js
@@ -0,0 +1,57 @@
+(function(){
+   "use strict";
+
+   var Moosipurk = function(){
+
+     // SEE ON SINGLETON PATTERN
+     if(Moosipurk.instance){
+       return Moosipurk.instance;
+     }
+     //this viitab Moosipurk fn
+     Moosipurk.instance = this;
+
+     console.log('moosipurgi sees');
+
+     // KÕIK muuutujad, mida muudetakse ja on rakendusega seotud defineeritakse siin
+     this.click_count = 0;
+     console.log(this);
+
+     // Kui tahan Moosipurgile referenci siis kasutan THIS = MOOSIPURGI RAKENDUS ISE
+     this.init();
+   };
+
+   window.Moosipurk = Moosipurk; // Paneme muuutja külge
+
+
+   // Kõik funktsioonid lähevad Moosipurgi külge
+   Moosipurk.prototype = {
+
+     init: function(){
+       console.log('Rakendus läks tööle');
+
+        // esimene loogika oleks see, et kuulame hiireklikki nupul
+        this.bindMouseEvents();
+
+     },
+
+     bindMouseEvents: function(){
+       document.querySelector('.add-new-jar').addEventListener('click', this.addNewClick.bind(this));
+
+     },
+
+     addNewClick: function(event){
+       console.log(event);
+
+       this.click_count++;
+       console.log(this.click_count);
+
+     }
+
+   };
+
+   // kui leht laetud käivitan Moosipurgi rakenduse
+   window.onload = function(){
+     var app = new Moosipurk();
+   };
+
+})();
